e1b2ff438f99de1dc1f9de203c67c004a15426b3
Mina Smart <mina.smart@shopify.com>
2016-03-02 13:45:32 -0500

    Attempt to make fetch isomorphic

--- shims/fetch.js
+++ shims/fetch.js
@@ -1,5 +1,14 @@
-const fetch = window.fetch;
-const Response = window.Response;
+/* global global, require */
+
+const globalNamespace = global || window;
+
+let fetch = globalNamespace.fetch;
+let Response = globalNamespace.Response;
+
+if (!fetch && !window && require) {
+  fetch = require('node-fetch');
+  Response = fetch.Response;
+}
 
 export { Response };
 export default fetch;
