6a4c944c27ada890b7a7c2b2b795a5796714b2b8
Mina Smart <mina.smart@shopify.com>
2016-03-02 14:12:43 -0500

    Safely check if we're in a node-like environment

--- src/isomorphic-fetch.js
+++ src/isomorphic-fetch.js
@@ -4,7 +4,14 @@ const globalNamespace = global || window;
 
 const fetch = globalNamespace.fetch;
 
-if (!fetch && !window && require) {
+function isNode() {
+  const windowAbsent = typeof window === 'undefined';
+  const requirePresent = typeof require === 'function';
+
+  return windowAbsent && requirePresent;
+}
+
+if (!fetch && isNode()) {
   globalNamespace.fetch = require('node-fetch');
   globalNamespace.Response = globalNamespace.fetch.Response;
 }
