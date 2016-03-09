157dc462200753a27b0d10beca2b2c7f4485b901
Tessa Thornton <tessa.thornton@shopify.com>
2016-03-09 11:48:08 -0500

    add yuidocs to jekyll

---
+++ api/api.js
@@ -0,0 +1,35 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Config",
+        "ShopClient",
+        "ShopifyBuy"
+    ],
+    "modules": [
+        "config",
+        "js-buy-sdk",
+        "shop-client",
+        "shopify"
+    ],
+    "allModules": [
+        {
+            "displayName": "config",
+            "name": "config"
+        },
+        {
+            "displayName": "js-buy-sdk",
+            "name": "js-buy-sdk"
+        },
+        {
+            "displayName": "shop-client",
+            "name": "shop-client"
+        },
+        {
+            "displayName": "shopify",
+            "name": "shopify",
+            "description": "This namespace contains all globally accessible classes"
+        }
+    ],
+    "elements": []
+} };
+});
