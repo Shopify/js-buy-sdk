3ad0207f47db1a6888b1a08cad782698206af757
Tessa Thornton <tessa.thornton@shopify.com>
2016-03-10 12:51:03 -0500

    lol whitespace

--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
-# JS Buy SDK Docs
+# [Shopify](https://www.shopify.com) JavaScript Buy SDK
+[![Circle CI](https://circleci.com/gh/Shopify/js-buy-sdk.png?circle-token=3be0ebe6fbb4841442b86678696947bd4b5456d7)](https://circleci.com/gh/Shopify/js-buy-sdk)
 
-## Local Setup
-- `gem install jekyll`
-- `npm install`
-- `script/update`
-- `jekyll serve`
+The JS Buy SDK is a lightweight library that allows you to build ecommerce into
+any website. It's based on Shopify's JSON API and provides the ability to
+retrieve products and collections from your shop, add products to a cart, and
+checkout.
 
-## Deployment
-Deployment to gh-pages branch will automatically trigger a jekyll build and deploy to http://shopify.github.io/js-buy-sdk/
+If you're just getting started, please [read the docs](http://shopify.github.io/js-buy-sdk/).
+
+## Contributing
+For help on setting up the repo locally, building, testing, and contributing
+please see [CONTRIBUTING.md](https://github.com/Shopify/js-buy-sdk/blob/master/CONTRIBUTING.md).
+
+## Code of Conduct
+All developers who wish to contribute through code or issues, take a look at the
+[CODE_OF_CONDUCT.md](https://github.com/Shopify/js-buy-sdk/blob/master/CODE_OF_CONDUCT.md).
---
+++ src/adapters/checkout-adapter.js
@@ -0,0 +1,87 @@
+import ajax from '../ajax';
+import CoreObject from '../metal/core-object';
+
+const CheckoutAdapter = CoreObject.extend({
+  ajax,
+
+  constructor(config) {
+    this.config = config;
+  },
+
+  get base64ApiKey() {
+    return btoa(this.config.apiKey);
+  },
+
+  get baseUrl() {
+    const { myShopifyDomain } = this.config;
+
+    return `https://${myShopifyDomain}.myshopify.com/anywhere`;
+  },
+
+  get headers() {
+    return {
+      Authorization: `Basic ${this.base64ApiKey}`,
+      'Content-Type': 'application/json'
+    };
+  },
+
+  idKeyForType() {
+    return 'token';
+  },
+
+  pathForType(type) {
+    return `/${type}`;
+  },
+
+  buildUrl(singleOrMultiple, type, idOrQuery) {
+    switch (singleOrMultiple) {
+        case 'multiple':
+          return this.buildMultipleUrl(type, idOrQuery);
+        case 'single':
+          return this.buildSingleUrl(type, idOrQuery);
+        default:
+          return '';
+    }
+  },
+
+  buildMultipleUrl(type, query = {}) {
+    const url = `${this.baseUrl}${this.pathForType(type)}.json`;
+    const paramNames = Object.keys(query);
+
+    if (paramNames.length > 0) {
+      throw new Error('Querying checkouts is not allowed');
+    }
+
+    return url;
+  },
+
+  buildSingleUrl(type, id) {
+    return `${this.baseUrl}${this.pathForType(type)}/${id}.json`;
+  },
+
+  fetchSingle(/* type, id */) {
+    const url = this.buildUrl('single', ...arguments);
+
+    return this.ajax('GET', url, { headers: this.headers }).then(response => {
+      return response.json;
+    });
+  },
+
+  create(type, payload) {
+    const url = this.buildUrl('multiple', type);
+
+    return this.ajax('POST', url, { headers: this.headers, body: JSON.stringify(payload) }).then(response => {
+      return response.json;
+    });
+  },
+
+  update(type, id, payload) {
+    const url = this.buildUrl('single', type, id);
+
+    return this.ajax('PATCH', url, { headers: this.headers, body: JSON.stringify(payload) }).then(response => {
+      return response.json;
+    });
+  }
+});
+
+export default CheckoutAdapter;
