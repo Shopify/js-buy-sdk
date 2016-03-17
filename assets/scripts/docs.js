a68cc7d22954d829b2d5e6cd55c83014dcdd7966
Haris Mahmood <harismahmood89@gmail.com>
2016-03-17 15:01:51 -0400

    docs sidebar subnav items

--- assets/scripts/docs.js
+++ assets/scripts/docs.js
@@ -7,12 +7,37 @@ $(function() {
       $('.docs-sub-nav__link').removeClass('active');
       var subNavLinkId = pageUrl.slice(indexOfOcto);
       $(subNavLinkId + '-link').addClass('active');
-    }
-  }
+    };
+  };
   highlightSubNavItem();
 
   $(window).bind( 'hashchange', function(e) {
     highlightSubNavItem();
   });
+
+
+  var generateApiClassNavItems = function () {
+    var pageUrl = window.location.href;
+    var items = window.YUIDocs.classes.map(function(docClass){
+      var activeClass = "";
+      if (pageUrl.includes(docClass.url.slice(2))) {
+        activeClass = "active";
+      }
+      return "<li><a href='" +
+        docClass.url +
+        "' class='docs-sub-nav__link " +
+        activeClass +
+        "'>" +
+        docClass.name +
+        "</a></li>"
+    });
+
+
+    var html = "<ul class='docs-sub-nav'>" + items.join('') + "</ul>";
+    $('.nav-item--reference').append(html);
+  };
+  if (window.YUIDocs) {
+    generateApiClassNavItems();
+  }
 
 });
