3cf51312b94879e729d2c09971b1ddba8e3ff168
Mina Smart <mina.smart@shopify.com>
2016-03-22 13:58:36 -0700

    Use polyfills in examples, and try for ie9 magic

--- src/ajax.js
+++ src/ajax.js
@@ -24,8 +24,39 @@ function parseResponse(response) {
     });
 }
 
+function ieFallback(method, url, opts) {
+  return new Promise(function (resolve, reject) {
+    const xdr = new XDomainRequest();
+
+    xdr.onload = function () {
+      try {
+        const json = JSON.parse(xdr.responseText);
+
+        resolve({ json, originalResponse: xdr, isJSON: true });
+      } catch (e) {
+        resolve({ text: xdr.responseText, originalResponse: xdr, isText: true });
+      }
+    };
+
+    function handleError() {
+      reject(new Error('There was an error with the XDR'));
+    }
+
+    xdr.onerror = handleError;
+    xdr.ontimeout = handleError;
+
+    xdr.open(method, url);
+    xdr.send(opts.data);
+  });
+}
+
 export default function ajax(method, url, opts = {}) {
+  if (window.XDomainRequest) {
+    return ieFallback(...arguments);
+  }
+
   opts.method = method;
+  opts.mode = 'cors';
 
   return fetch(url, opts)
     .then(checkStatus)
