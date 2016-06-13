3b43c05addb6f6c55f0fe715aa6307166a4fc62a
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-06-13 15:51:48 -0400

    added promise shim to test node-lib directory

--- scripts/node-qunit.js
+++ scripts/node-qunit.js
@@ -12,6 +12,9 @@ const originalRequire = Module.prototype.require;
 global.QUnit = require('qunitjs');
 global.localStorage = require('node-localstorage').LocalStorage(`${rootDirectory}tmp`);
 
+// used in promise shim
+global.RSVP = require('rsvp');
+
 // used by pretender
 global.self = {
   RouteRecognizer: require('route-recognizer'),
@@ -24,34 +27,34 @@ let index = 1;
 let tapMessage = '';
 
 Module.prototype.require = function () {
-  const shopifyBuySrcNamespace = 'shopify-buy';
-  const shopifyBuyTestsNamespace = 'shopify-buy/tests';
-  const qunitShimName = 'qunit';
-  const requiresShopifyBuySrc = arguments[0].indexOf(shopifyBuySrcNamespace) === 0;
-  const requiresShopifyBuyTest = arguments[0].indexOf(shopifyBuyTestsNamespace) === 0;
-  const requiresQunitShim = arguments[0] === qunitShimName;
+  const namespace = {
+    src: { prefixes: ['shopify-buy'], isRequired:false, dirName: 'src'},
+    tests: { prefixes: ['shopify-buy/tests'], isRequired:false, dirName: 'tests'},
+    shims: { prefixes: ['qunit', 'promise'], isRequired:false, dirName: './'},
+  };
+  const libDirName = 'node-lib';
+  const requiredPackage = arguments[0];
 
-  if (requiresShopifyBuyTest || requiresShopifyBuySrc || requiresQunitShim) {
-    const libDirectoryName = 'node-lib';
-    const srcDirectoryName = 'src';
-    const testsDirectoryName = 'tests';
-    const currentDirectoryFullPath = path.dirname(this.id);
+  namespace.src.isRequired = requiredPackage.indexOf(namespace.src.prefixes[0]) === 0;
+  namespace.tests.isRequired = requiredPackage.indexOf(namespace.tests.prefixes[0]) === 0;
+  namespace.shims.isRequired = namespace.shims.prefixes.indexOf(requiredPackage) !== -1;
 
-    const distDirectoryFullPath = currentDirectoryFullPath.substring(0, currentDirectoryFullPath.indexOf(libDirectoryName));
-    const srcDirectoryFullPath = path.join(distDirectoryFullPath, libDirectoryName, srcDirectoryName);
-    const srcDirectoryRelativePath = path.relative(currentDirectoryFullPath, srcDirectoryFullPath);
-    const testsDirectoryFullPath = path.join(distDirectoryFullPath, libDirectoryName, testsDirectoryName);
-    const testsDirectoryRelativePath = path.relative(currentDirectoryFullPath, testsDirectoryFullPath);
+  if (namespace.src.isRequired || namespace.tests.isRequired || namespace.shims.isRequired) {
+    const currentDirName = path.dirname(this.id);
 
-    if (requiresQunitShim) {
+    const distDirectoryFullPath = currentDirName.substring(0, currentDirName.indexOf(libDirName));
+    const srcDirectoryFullPath = path.join(distDirectoryFullPath, libDirName, namespace.src.dirName);
+    const srcDirectoryRelativePath = path.relative(currentDirName, srcDirectoryFullPath);
+    const testsDirectoryFullPath = path.join(distDirectoryFullPath, libDirName, namespace.tests.dirName);
+    const testsDirectoryRelativePath = path.relative(currentDirName, testsDirectoryFullPath);
+
+    if (namespace.shims.isRequired) {
       arguments[0] = path.join(testsDirectoryRelativePath, arguments[0]);
-    } else if (requiresShopifyBuyTest) { //order matters here. `shopify-buy/tests` must match before `shopify-buy`
-      arguments[0] = arguments[0].replace(shopifyBuyTestsNamespace, testsDirectoryRelativePath);
-    } else if (requiresShopifyBuySrc) {
-      arguments[0] = arguments[0].replace(shopifyBuySrcNamespace, srcDirectoryRelativePath);
+    } else if (namespace.tests.isRequired) { //order matters here. `shopify-buy/tests` must match before `shopify-buy`
+      arguments[0] = arguments[0].replace(namespace.tests.prefixes[0], testsDirectoryRelativePath);
+    } else if (namespace.src.isRequired) {
+      arguments[0] = arguments[0].replace(namespace.src.prefixes[0], srcDirectoryRelativePath);
     }
-
-
   }
 
   return originalRequire.apply(this, arguments);
