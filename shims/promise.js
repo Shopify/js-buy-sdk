bdf1f2afd2a84e38fca7733745d3957645f304ec
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-08 14:48:55 -0400

    rewrote the promise shim to be usable in a node environment.

--- shims/promise.js
+++ shims/promise.js
@@ -1,4 +1,21 @@
-const RSVP = window.RSVP;
+/* globals require */
+
+function isNodeLikeEnvironment() {
+  const windowAbsent = typeof window === 'undefined';
+  const requirePresent = typeof require === 'function';
+
+  return windowAbsent && requirePresent;
+}
+
+let RSVP;
+
+if (isNodeLikeEnvironment()) {
+  const localRequire = require;
+
+  RSVP = localRequire('rsvp');
+} else {
+  RSVP = window.RSVP;
+}
 const Promise = RSVP.Promise;
 
 export { RSVP, Promise };
