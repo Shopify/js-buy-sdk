65318eb55feaf615926e65cba1579855ce4eb387
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-08 15:44:43 -0400

    Rewrote functions to follow js standards and correct use of js apis

--- build-lib/util/recursive-read-dir.js
+++ build-lib/util/recursive-read-dir.js
@@ -3,20 +3,19 @@
 const fs = require('fs');
 const path = require('path');
 
-function recursiveReadDir (dir) {
+function recursiveReadDir(dir) {
 
   const filesArray = fs.readdirSync(dir).map(fileName => {
     const file = path.join(dir, fileName);
 
     if (fs.statSync(file).isDirectory()) {
-
-      return files.concat(recursiveReadDir(file));
+      return recursiveReadDir(file);
     }
 
     return file;
   });
 
-  return [].concat(...filesArray)
+  return [].concat(...filesArray);
 }
 
 module.exports = recursiveReadDir;
