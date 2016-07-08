7068c7b05e99865c776fbae6dd48f14bdf3fcf15
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-08 14:36:05 -0400

    Added a module for recursive writing of files when a path relative to a non-existent directory is provided

---
+++ build-lib/util/recursive-write-file.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+
+function ensureDirExists (fullPath) {
+
+  if (!fs.existsSync(fullPath)) {
+    if (ensureDirExists(path.dirname(fullPath))) {
+      fs.mkdirSync(fullPath);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = function (fullPath, content) {
+  if(ensureDirExists(path.dirname(fullPath))) {
+    fs.writeFileSync(fullPath, content);
+    return true;
+  }
+
+  return false;
+};
