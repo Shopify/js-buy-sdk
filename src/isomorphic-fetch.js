84581e6b30b9cc22d32e1bded287f15f4102ff0c
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-11 11:57:50 -0400

    Added comments to describe why global.require is not used in isomorphic-fetch

--- src/isomorphic-fetch.js
+++ src/isomorphic-fetch.js
@@ -6,6 +6,9 @@ import isNodeLikeEnvironment from './metal/is-node-like-environment';
 const fetch = global.fetch;
 
 if (!fetch && isNodeLikeEnvironment()) {
+  /* global.require is only available in the REPL 
+   * and Babel fails when you use require() explicitly
+   */
   const localRequire = require;
 
   global.fetch = localRequire('node-fetch');
