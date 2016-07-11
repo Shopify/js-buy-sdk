a09b69fcd0d64b5500d2e907b9aaca998aac30f8
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-11 11:03:03 -0400

    Updated isomorphic-fetch's use of global.require because it breaks in node environment

--- src/isomorphic-fetch.js
+++ src/isomorphic-fetch.js
@@ -1,9 +1,13 @@
+/* globals require */
+
 import global from './metal/global';
 import isNodeLikeEnvironment from './metal/is-node-like-environment';
 
 const fetch = global.fetch;
 
 if (!fetch && isNodeLikeEnvironment()) {
-  global.fetch = global.require('node-fetch');
+  const localRequire = require;
+
+  global.fetch = localRequire('node-fetch');
   global.Response = global.fetch.Response;
 }
