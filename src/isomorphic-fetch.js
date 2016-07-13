e0a0e7479dabcab685ea2491e936822a542ecf2b
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-13 15:33:18 -0400

    Updated comment that describes why an inderection is needed for require with amd

--- src/isomorphic-fetch.js
+++ src/isomorphic-fetch.js
@@ -6,8 +6,8 @@ import isNodeLikeEnvironment from './metal/is-node-like-environment';
 const fetch = global.fetch;
 
 if (!fetch && isNodeLikeEnvironment()) {
-  /* global.require is only available in the REPL
-   * and Babel fails when you use require() explicitly
+  /* this indirection is needed because babel throws errors when
+   * transpiling require('node-fetch') using `amd` plugin with babel6
    */
   const localRequire = require;
 
