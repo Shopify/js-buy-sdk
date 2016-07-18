89590a89290e58f8ecb6fc2f11b7ba2ed64eb426
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-07-18 17:46:23 -0400

    Promisified scripts/util/yuidoc.js

--- scripts/util/yuidoc.js
+++ scripts/util/yuidoc.js
@@ -1,6 +1,7 @@
 var yuidoc = require('yuidocjs');
 var fs = require('fs');
 var path = require('path');
+var util = require('util');
 
 /** prepare options in format required by YUIDOC
   * It is based on logic in yuidoc.Project.init
@@ -8,42 +9,45 @@ var path = require('path');
 var project = yuidoc.Files.getJSON('./yuidoc.json');
 var options = project.options;
   delete project.options;
+  options.paths = [];
   options.project = project;
   options.nocode = true;
   options.quiet = true;
   options.themedir = path.dirname(require.resolve(path.join('yuidoc-lucid-theme', 'theme.json')));
   options.helpers = [path.join(options.themedir, 'helpers', 'helpers.js')];
 
-function buildDocSync(directory, rest, callback) {
-  options.paths = [ path.join(directory, 'src') ];
-  options.outdir = path.join(directory, 'api');
-  options.project.version = path.basename(directory);
-
-  console.log('Generating docs for ' + options.project.version, 'info', 'yuidoc');
-
-  var json = (new yuidoc.YUIDoc(options)).run();
-  var builder = new yuidoc.DocBuilder(options, json);
-  builder.compile(function () {
-    if (rest.length) {
-      buildDocSync(rest.shift(), rest, callback);
-    } else {
-      callback();
-    }
-  });
-}
-
 module.exports = {
-  generate: function (_options, callback) {
+  generate: function (_options) {
+    options = util._extend(options, _options);
     var starttime = (new Date()).getTime();
-    var versionPaths = _options.paths;
-    options.themedir = _options.themeDir || options.themedir;
+    var promise = Promise.resolve();
 
-    buildDocSync(versionPaths.shift(), versionPaths, function () {
+    options.paths.forEach((currentPath) => {
+      var currentOptions = JSON.parse(JSON.stringify(options));
+      currentOptions = util._extend(currentOptions, {
+        paths: [ path.join(currentPath, 'src') ],
+        outdir: path.join(currentPath, 'api'),
+        project: {
+          version: path.basename(currentPath)
+        }
+      });
+
+      var json = (new yuidoc.YUIDoc(currentOptions)).run();
+      var builder = new yuidoc.DocBuilder(currentOptions, json);
+
+      promise = promise.then(() => {
+        return new Promise((resolve, reject) => {
+          console.info('info: Generating docs for ' + currentOptions.project.version, 'info', 'yuidoc');
+          builder.compile(function () {
+            resolve();
+          });
+        })
+      });
+    });
+
+    return promise.then(() => {
       var endtime = (new Date()).getTime();
-      yuidoc.log('Completed in ' + ((endtime - starttime) / 1000) + ' seconds', 'info', 'yuidoc');
-      if (callback) {
-        callback()
-      }
+      console.info('info: Completed in ' + ((endtime - starttime) / 1000) + ' seconds', 'info', 'yuidoc');
     });
   }
 }
