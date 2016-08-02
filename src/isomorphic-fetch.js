2b38a5574cdc3491e259594684ad01af47be9da7
Abayomi Osamiluyi <yomexzo@gmail.com>
2016-08-02 14:38:12 -0400

    Updated isomorphic-fetch's use of global.require because it breaks in node env (#164)
    * Updated isomorphic-fetch's use of global.require because it breaks in node environment
    
    * Added comments to describe why global.require is not used in isomorphic-fetch
    
    * removed trailing space in `isomorphic-fetch`
    
    * Updated comment that describes why an inderection is needed for require with amd

--- src/isomorphic-fetch.js
+++ src/isomorphic-fetch.js
@@ -1,9 +1,16 @@
+/* globals require */
+
 import global from './metal/global';
 import isNodeLikeEnvironment from './metal/is-node-like-environment';
 
 const fetch = global.fetch;
 
 if (!fetch && isNodeLikeEnvironment()) {
-  global.fetch = global.require('node-fetch');
+  /* this indirection is needed because babel throws errors when
+   * transpiling require('node-fetch') using `amd` plugin with babel6
+   */
+  const localRequire = require;
+
+  global.fetch = localRequire('node-fetch');
   global.Response = global.fetch.Response;
 }
