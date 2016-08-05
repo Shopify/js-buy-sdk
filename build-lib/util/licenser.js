7c44cb09325294f1c2f23d6892b5063684209896
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-08-05 16:32:10 -0400

    Added code/snippet for Mina to help debug

--- build-lib/util/licenser.js
+++ build-lib/util/licenser.js
@@ -27,6 +27,51 @@ Licenser.prototype.constructor = Licenser;
 
 Licenser.prototype.build = function () {
 
+  let self = this; // pretend like you did not see this.
+
+  return Promise.resolve().then(() => {
+    return new Promise((resolve, reject) => {
+      let files = [];
+
+      fsExtra.walk(self.inputPaths[0]).on('data', file => {
+        files.push(file.path)
+      }).on('end', () => {
+        resolve(files);
+      });
+    })
+  }).then(function(foundFiles){
+    console.log(foundFiles); // I expect a list of all files here. Only the root directory is listed
+    console.log(recursiveReadDir(self.inputPaths[0])); //all files are listed here. which means the directory was not empty
+
+
+    // this is what we have before. Runs anyways.
+    const fileDescriptions = [].concat(...self.inputPaths.map(dirname => {
+      return recursiveReadDir(dirname).map(fileName => {
+        return {
+          baseDirectory: dirname,
+          fileName: fileName
+        }
+      });
+    }));
+
+    fileDescriptions.forEach(description => {
+      const inputBuffer = fs.readFileSync(path.join(description.fileName));
+      let outputBuffer;
+
+      if (description.fileName.match(/^.+\.js$/)) {
+        outputBuffer = `/*\n${LICENSE}*/\n\n${inputBuffer}`;
+      } else {
+        outputBuffer = inputBuffer;
+      }
+
+      const destination = description.fileName.replace(description.baseDirectory, self.outputPath);
+
+      mkdirp(path.dirname(destination));
+      fs.writeFileSync(destination, outputBuffer);
+    });
+
+  });
+
   const fileDescriptions = [].concat(...this.inputPaths.map(dirname => {
     return recursiveReadDir(dirname).map(fileName => {
       return {
