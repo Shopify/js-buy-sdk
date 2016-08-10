d516b65cc9d5d07a22bb086326491d302b2d2357
mikkoh <mikko.haapoja@shopify.com>
2016-08-10 14:17:10 -0400

    Implemented dropping replacing '{{versionString}}' with version

--- scripts/addHeaderToSource.js
+++ scripts/addHeaderToSource.js
@@ -1,16 +1,28 @@
+'use strict';
+
 const path = require('path');
 const fs = require('fs');
 
 const getVersion = require('./getVersion');
 
-getVersion((version) => {
+getVersion((err, version) => {
+  if(err) {
+    throw err;
+  }
+
   const pathSource = path.join(process.cwd(), process.argv[ 2 ]);
   const pathLicense = path.join(process.cwd(), 'LICENSE.txt');
 
-  const source = fs.readFileSync(pathSource, 'utf8');
   const license = fs.readFileSync(pathLicense, 'utf8');
-
   const header = `/*\n${license}\n*/\n\n/* version: ${version} */`;
+
+  let source = fs.readFileSync(pathSource, 'utf8');
+
+  // this is not nice to do here but it's quick and dirty
+  // within the source of the app theres a string like
+  // {{versionString}}
+  // we want to replace that with the generated version
+  source = source.replace('{{versionString}}', version);
 
   fs.writeFileSync(pathSource, `${header}\n\n${source}`);
 });
