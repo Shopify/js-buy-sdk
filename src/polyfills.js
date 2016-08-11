70717d67d1e6f1cf8b63eefb8403659c6ad7b846
mikkoh <mikko.haapoja@shopify.com>
2016-08-11 11:58:01 -0400

    made polyfills.js pass linting

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,3 @@
-// import promisePoly from 'rsvp/lib/rsvp.js';
 import promisePoly from 'core-js/fn/promise';
 import 'whatwg-fetch/fetch.js';
 import 'Base64/base64.js';
@@ -7,13 +6,13 @@ let scope;
 
 // check if we're running in an eco-system that uses window
 // if it doesnt exist use global
-if(typeof window !== 'undefined') {
+if (typeof window !== 'undefined') {
   scope = window;
 } else {
   scope = global;
 }
 
 // write polyfills to global scope
-if(scope.Promise === undefined) {
+if (!scope.Promise) {
   scope.Promise = promisePoly;
 }
