f004d692058978928403aaa8bbec3849488ec07a
mikkoh <mikko.haapoja@shopify.com>
2016-08-17 13:58:35 -0400

    Removed code from my bad copy and paste job :snake:

--- .rollup.config.umd.polfilled.js
+++ .rollup.config.umd.polfilled.js
@@ -13,24 +13,9 @@ export default {
     }),
 
     commonjs({
-      // non-CommonJS modules will be ignored, but you can also
-      // specifically include/exclude files
-      include: 'node_modules/**',  // Default: undefined
-      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-
-      // search for files other than .js files (must already
-      // be transpiled by a previous plugin!)
-      extensions: [ '.js', '.coffee' ],  // Default: [ '.js' ]
-
-      // if true then uses of `global` won't be dealt with by this plugin
-      ignoreGlobal: false,  // Default: false
-
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false,  // Default: true
-
-      // explicitly specify unresolvable named exports
-      // (see below for more details)
-      namedExports: { './module.js': ['foo', 'bar' ] }  // Default: undefined 
+      include: 'node_modules/**',
+      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
+      sourceMap: false
     })
   ]
 };
