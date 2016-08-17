809b622c787ded2425e3de4dcaa2103a87f8b1f2
mikkoh <mikko.haapoja@shopify.com>
2016-08-17 16:07:28 -0400

    Switched checking global to use the global module which does checking

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,16 +1,7 @@
 import promisePoly from 'core-js/fn/promise';
 import 'whatwg-fetch/fetch.js';
 import 'Base64/base64.js';
-
-let scope;
-
-// check if we're running in an eco-system that uses window
-// if it doesnt exist use global
-if (typeof window !== 'undefined') {
-  scope = window;
-} else {
-  scope = global;
-}
+import scope from './metal/global';
 
 // write polyfills to global scope
 if (!scope.Promise) {
