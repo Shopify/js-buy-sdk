1c5c971207f7c93e0e6180c710c6395f976fee82
Yomi Osamiluyi <yomi.osamiluyi@shopify.com>
2016-08-31 16:03:33 -0400

    Generate latest documentation into `/docs/api`

--- scripts/util/yuidoc.js
+++ scripts/util/yuidoc.js
@@ -1,45 +1,38 @@
-var yuidoc = require('yuidocjs');
-var fs = require('fs');
-var path = require('path');
-var util = require('util');
+"use strict";
 
-/** prepare options in format required by YUIDOC
-  * It is based on logic in yuidoc.Project.init
-  */
-var project = yuidoc.Files.getJSON('./yuidoc.json');
-var options = project.options;
+const path = require('path');
 
-delete project.options;
+const yuidoc = require('yuidocjs');
+const project = yuidoc.Files.getJSON('./yuidoc.json');
+const options = project.options;
+
 options.paths = [];
 options.project = project;
+delete options.project.options;
 
 module.exports = {
-  generate: function (_options) {
-    options = Object.assign({}, options, _options);
-    var starttime = (new Date()).getTime();
-    var promises = options.paths.map((currentPath) => {
-      var currentOptions = JSON.parse(JSON.stringify(options));
-      Object.assign(currentOptions, {
-        paths: [ path.join(currentPath, 'src') ],
-        outdir: currentPath,
-        project: {
-          version: path.basename(currentPath)
-        }
-      });
+  generate: function (apiDocsMeta) {
+    const starttime = (new Date()).getTime();
+    const promises = apiDocsMeta.map((meta) => {
+      const currentOptions = JSON.parse(JSON.stringify(options));
 
-      var json = (new yuidoc.YUIDoc(currentOptions)).run();
-      var builder = new yuidoc.DocBuilder(currentOptions, json);
+      currentOptions.project.version = meta.version;
+      currentOptions.paths = [ path.join(meta.srcRelativePath, 'src') ];
+      currentOptions.outdir = meta.apiPath;
+
+      const json = (new yuidoc.YUIDoc(currentOptions)).run();
+      const builder = new yuidoc.DocBuilder(currentOptions, json);
 
       return new Promise((resolve, reject) => {
         console.info('info: Generating docs for ' + currentOptions.project.version, 'info', 'yuidoc');
         builder.compile(function () {
           resolve();
         });
-      })
+      });
     });
 
     return Promise.all(promises).then(() => {
-      var endtime = (new Date()).getTime();
+      const endtime = (new Date()).getTime();
       console.info('info: Completed in ' + ((endtime - starttime) / 1000) + ' seconds', 'info', 'yuidoc');
     });
   }
