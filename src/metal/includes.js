ea8be2c9250ca48a75b176a77a72096bdc5290cb
Tom Watson <tom.james.watson@googlemail.com>
2016-09-03 00:03:08 +0100

    fix includes bug where element begins with number

--- src/metal/includes.js
+++ src/metal/includes.js
@@ -1,46 +1,34 @@
-let includes;
+export default function (array, searchElement) {
+  const ObjectifiedArray = Object(array);
+  const length = parseInt(ObjectifiedArray.length, 10) || 0;
 
-if (!Array.prototype.includes) {
-  includes = function (array, searchElement) {
-    const ObjectifiedArray = Object(array);
-    const length = parseInt(ObjectifiedArray.length, 10) || 0;
-
-    if (length === 0) {
-      return false;
-    }
-
-    const startIndex = parseInt(arguments[1], 10) || 0;
-    let index;
-
-    if (startIndex >= 0) {
-      index = startIndex;
-    } else {
-      index = length + startIndex;
-
-      if (index < 0) {
-        index = 0;
-      }
-    }
-
-    while (index < length) {
-      const currentElement = ObjectifiedArray[index];
-
-      /* eslint no-self-compare:0 */
-      if (searchElement === currentElement ||
-         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-        return true;
-      }
-      index++;
-    }
-
+  if (length === 0) {
     return false;
-  };
-} else {
-  includes = function (array) {
-    const args = [].slice.call(arguments, 1);
+  }
 
-    return Array.prototype.includes.apply(array, args);
-  };
+  const startIndex = parseInt(arguments[2], 10) || 0;
+  let index;
+
+  if (startIndex >= 0) {
+    index = startIndex;
+  } else {
+    index = length + startIndex;
+
+    if (index < 0) {
+      index = 0;
+    }
+  }
+
+  while (index < length) {
+    const currentElement = ObjectifiedArray[index];
+
+    /* eslint no-self-compare:0 */
+    if (searchElement === currentElement ||
+       (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+      return true;
+    }
+    index++;
+  }
+
+  return false;
 }
-
-export default includes;
