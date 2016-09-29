99452a613484f19b1e96ad1d3100d9478ab72523
mikkoh <mikko.haapoja@shopify.com>
2016-09-29 13:51:57 -0400

    Explicitely adding btoa and atob to window scope

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,9 +1,18 @@
-import promisePoly from 'core-js/fn/promise';
 import 'whatwg-fetch/fetch.js';
-import 'Base64/base64.js';
+import promisePoly from 'core-js/fn/promise';
+import base64 from 'Base64/base64.js';
 import scope from './metal/global';
 
-// write polyfills to global scope
+// drop in polyfills from base64
+if (!scope.btoa) {
+  scope.btoa = base64.btoa;
+}
+
+if (!scope.atob) {
+  scope.atob = base64.atob;
+}
+
+// drop in polyfills from Promise
 if (!scope.Promise) {
   scope.Promise = promisePoly;
 }
