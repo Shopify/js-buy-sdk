3fb40642ad1e2b446d60e7c87e4829370a48e481
mikkoh <mikko.haapoja@shopify.com>
2016-10-03 11:12:38 -0400

    Added an add function to metal/global this ensures that Babel properly transpiles the polyfill code and does not delete it :(

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,20 +1,11 @@
 import 'whatwg-fetch/fetch.js';
 import promisePoly from 'core-js/fn/promise';
 import base64 from 'Base64/base64.js';
-import scope from './metal/global';
-
-console.log(scope);
+import scope, { add } from './metal/global';
 
 // drop in polyfills from base64
-if (!scope.btoa) {
-  scope.btoa = base64.btoa;
-}
-
-if (!scope.atob) {
-  scope.atob = base64.atob;
-}
+add('btoa', base64.btoa);
+add('atob', base64.atob);
 
 // drop in polyfills from Promise
-if (!scope.Promise) {
-  scope.Promise = promisePoly;
-}
+add('Promise', promisePoly);
