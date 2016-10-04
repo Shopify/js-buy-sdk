52f43c6e5671e3c9260ed1d6badef416c9f2d6ac
mikkoh <mikko.haapoja@shopify.com>
2016-10-04 12:59:33 -0400

    Circle ci does not test against a git repo so version checker will return something like 'v1.0.1-no commit'

--- tests/unit/api/version-test.js
+++ tests/unit/api/version-test.js
@@ -9,5 +9,5 @@ test('it exists', function (assert) {
   assert.expect(2);
 
   assert.equal(typeof version, 'string');
-  assert.ok(version.match(/v\d\.\d\.\d-[a-f0-9]{6}/), `a version string of the format vX.Y.Z-abc123 exists. Found ${version}`);
+  assert.ok(version.match(/v\d\.\d\.\d-([a-f0-9]{6}|no commit)/), `a version string of the format vX.Y.Z-abc123 exists. Found ${version}`);
 });
