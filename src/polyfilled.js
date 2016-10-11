d3de220dfd52d26036be6d27320dff91b244ff04
Mikko Haapoja <mikkoh@users.noreply.github.com>
2016-10-11 14:42:04 -0400

    Fix Webpack + Node + add Polyfill dist on NPM (#236)
    * Made it so that node-fetch is not distributed for browser builds. This means that webpack builds will work. Also there's a file called polyfilled.js in the root which will allow you to do something like require('shopify-buy/polyfilled'). Fixes #213
    
    * Fixed module version for get-project-version
    
    * Circle ci does not test against a git repo so version checker will return something like 'v1.0.1-no commit'
    
    * Updated get-project-version
    
    * Revered npmignore
    
    * Removed test for 'no commit' on version
    
    * Actually build the umd-builds when serving examples and watch
    
    * Import relative paths in the same dir

---
+++ src/polyfilled.js
@@ -0,0 +1,8 @@
+/**
+ * It should be noted that this script will be transpiled into the root of
+ * the js-buy-sdk on prepublish
+ */
+import './polyfills';
+import shopify from './shopify';
+
+export default shopify;
