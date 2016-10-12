47e740a474acb67ff0195d048ebecbcc347e23d3
mikkoh <mikko.haapoja@shopify.com>
2016-10-12 11:29:58 -0400

    This fixed pathing issues on  in root of module

--- src/polyfilled.js
+++ src/polyfilled.js
@@ -2,7 +2,7 @@
  * It should be noted that this script will be transpiled into the root of
  * the js-buy-sdk on prepublish
  */
-import './polyfills';
-import shopify from './shopify';
+import './lib/polyfills';
+import shopify from './lib/shopify';
 
 export default shopify;
