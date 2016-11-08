b096b8f076027219575aad3c86d956ce00be7277
mikkoh <mikko.haapoja@shopify.com>
2016-11-08 14:26:30 -0500

    Both lib/ and jsnext builds are now being bundled into one file. Also updated jsnext build to be built into es/

--- src/polyfilled.js
+++
@@ -1,8 +0,0 @@
-/**
- * It should be noted that this script will be transpiled into the root of
- * the js-buy-sdk on prepublish
- */
-import './lib/polyfills';
-import shopify from './lib/shopify';
-
-export default shopify;
