0bd2f646940fa8f5a20bffc38bb766b5c72e1f04
Maximilian Beck <akkumulator1@gmail.com>
2016-11-10 08:14:43 +0100

    changed myShopifyDomain to domain
    Notice: myShopifyDomain is deprecated, please use domain and provide the full shop domain.

--- examples/cart/index.js
+++ examples/cart/index.js
@@ -4,7 +4,7 @@ $(function() {
   ============================================================ */
   var client = ShopifyBuy.buildClient({
     apiKey: 'bf081e860bc9dc1ce0654fdfbc20892d',
-    myShopifyDomain: 'embeds',
+    domain: 'embeds',
     appId: '6'
   });
 
