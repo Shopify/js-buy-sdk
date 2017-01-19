27d8708b9cc8ec41ee223ac02fea42c856763c48
Jessica Jiang <jessica.jiang@shopify.com>
2017-01-19 12:57:37 -0500

    Add base64 encoding check and remove empty line

--- src-graphql/base64.js
+++ src-graphql/base64.js
@@ -1,7 +1,9 @@
 export default function base64(str) {
   if (typeof btoa === 'function') {
     return btoa(str);
-  } else {
+  } else if (typeof Buffer !== 'undefined') { // eslint-disable-line no-negated-condition
     return Buffer.from(str).toString('base64');
+  } else {
+    throw new Error('No native way to base64 encode');
   }
 }
