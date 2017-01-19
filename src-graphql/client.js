9eafc74972e67f4c406430bcaca2d50efa75f70c
Jessica Jiang <jessica.jiang@shopify.com>
2017-01-19 13:16:56 -0500

    Rename base64 to base64encode

--- src-graphql/client.js
+++ src-graphql/client.js
@@ -1,11 +1,11 @@
 import GraphQLJSClient from '@shopify/graphql-js-client';
 import types from '../types';
-import base64 from './base64';
+import base64Encode from './base64encode';
 
 export default class Client {
   constructor(config, GraphQLClientClass = GraphQLJSClient) {
     const apiUrl = `https://${config.domain}/api/graphql`;
-    const authHeader = `Authorization: Basic ${base64(config.storefrontAccessToken)}`;
+    const authHeader = `Authorization: Basic ${base64Encode(config.storefrontAccessToken)}`;
 
     this.graphQLClient = new GraphQLClientClass(types, apiUrl, {headers: authHeader});
   }
