856a0e6bf1f5287aed80cfe71f98e7b362cce74b
Jessica Jiang <jessica.jiang@shopify.com>
2017-01-19 12:44:20 -0500

    Add fetch for products
    Change to exact plugin versions
    
    Fix isomorphic fetch mock and remove query testing
    
    Add missing fixture

--- scripts-graphql/rollup-tests.js
+++ scripts-graphql/rollup-tests.js
@@ -6,12 +6,14 @@ const multiEntry = require('rollup-plugin-multi-entry');
 const builtins = require('rollup-plugin-node-builtins');
 const globals = require('rollup-plugin-node-globals');
 const babel = require('rollup-plugin-babel');
-const remap = require('@shopify/rollup-plugin-remap').default;
+const json = require('rollup-plugin-json');
+const remap = require('@shopify/rollup-plugin-remap');
 const eslintTestGenerator = require('./rollup-plugin-eslint-test-generator');
 
 function envRollupInfo({browser, withDependencyTracking}) {
   const format = (browser) ? 'iife' : 'cjs';
   const plugins = [
+    json(),
     eslintTestGenerator({
       paths: [
         'src-graphql',
@@ -43,8 +45,16 @@ function envRollupInfo({browser, withDependencyTracking}) {
 
   if (browser) {
     plugins.unshift(globals(), builtins());
+    plugins.unshift(remap({
+      originalPath: './test-graphql/isomorphic-fetch-mock.js',
+      targetPath: './test-graphql/fetch-mock-browser.js'
+    }));
   } else {
     external.push('assert');
+    plugins.unshift(remap({
+      originalPath: './test-graphql/isomorphic-fetch-mock.js',
+      targetPath: './test-graphql/fetch-mock-node.js'
+    }));
   }
 
   return {plugins, external, format};
