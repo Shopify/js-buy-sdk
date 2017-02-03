170bfe85aa17075ce389dae059ead1f4073f6886
Jessica Jiang <jessica.jiang@shopify.com>
2017-02-03 15:35:05 -0500

    Add pagination for the fetch product functions
    Fetch all images/variants on products instead of fetching all products/collections
    
    Add image/variant pagination to fetchProduct

---
+++ fixtures/product-with-paginated-images-fixture.js
@@ -0,0 +1,79 @@
+export default {
+  "data": {
+    "shop": {
+      "products": {
+        "pageInfo": {
+          "hasNextPage": true,
+          "hasPreviousPage": false
+        },
+        "edges": [
+          {
+            "cursor": "eyJsYXN0X2lkIjo3ODU3OTg5Mzg0LCJsYXN0X3ZhbHVlIjoiNzg1Nzk4OTM4NCJ9",
+            "node": {
+              "id": "gid://shopify/Product/7857989384",
+              "createdAt": "2016-09-25T21:31:33Z",
+              "updatedAt": "2017-01-16T15:42:21Z",
+              "bodyHtml": "send me this cat",
+              "handle": "cat",
+              "productType": "",
+              "title": "Cat",
+              "vendor": "sendmecats",
+              "tags": [],
+              "publishedAt": "2016-09-25T21:29:00Z",
+              "options": [
+                {
+                  "id": "gid://shopify/ProductOption/9417004808",
+                  "name": "Fur",
+                  "values": [
+                    "Fluffy",
+                    "Extra Fluffy",
+                    "Mega Fluff"
+                  ]
+                }
+              ],
+              "images": {
+                "pageInfo": {
+                  "hasNextPage": true,
+                  "hasPreviousPage": false
+                },
+                "edges": [
+                  {
+                    "cursor": "eyJsYXN0X2lkIjoxNjMwNjgxMjY4MH0=",
+                    "node": {
+                      "id": "gid://shopify/ProductImage/16306812680",
+                      "src": "https://cdn.shopify.com/s/files/1/1510/7238/products/cat.jpg?v=1474839096",
+                      "altText": null
+                    }
+                  }
+                ]
+              },
+              "variants": {
+                "pageInfo": {
+                  "hasNextPage": false,
+                  "hasPreviousPage": false
+                },
+                "edges": [
+                  {
+                    "cursor": "eyJsYXN0X2lkIjoyNTYwMjIzNTk3Nn0=",
+                    "node": {
+                      "id": "gid://shopify/ProductVariant/25602235976",
+                      "title": "Fluffy",
+                      "price": "0.00",
+                      "weight": 18,
+                      "selectedOptions": [
+                        {
+                          "name": "Fur",
+                          "value": "Fluffy"
+                        }
+                      ]
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+};
--- src-graphql/client.js
+++ src-graphql/client.js
@@ -6,6 +6,7 @@ import productQuery from './product-query';
 import productConnectionQuery from './product-connection-query';
 import collectionQuery from './collection-query';
 import collectionConnectionQuery from './collection-connection-query';
+import fetchAll from './fetch-all';
 
 export default class Client {
   constructor(config, GraphQLClientClass = GraphQLJSClient) {
@@ -22,15 +23,53 @@ export default class Client {
     });
   }
 
+  fetchAllImagesOrVariants(type, product, allImagesOrVariants, promises) {
+    if (product[type].pageInfo.hasNextPage) {
+      promises.push(this.graphQLClient.send(allImagesOrVariants.nextPageQuery()).then((result) => {
+        allImagesOrVariants.push(...result.model.node[type]);
+
+        return fetchAll(type, allImagesOrVariants, result, this.graphQLClient);
+      }));
+    }
+  }
+
   fetchAllProducts(query = productConnectionQuery()) {
     return this.graphQLClient.send(query(this.graphQLClient)).then((response) => {
-      return response.model.shop.products;
+      const promises = [];
+
+      // Add all images and variants for each product
+      for (let i = 0; i < response.model.shop.products.length; i++) {
+        const productImages = response.model.shop.products[i].images;
+        const productVariants = response.model.shop.products[i].variants;
+
+        // Fetch the rest of the images for this product
+        this.fetchAllImagesOrVariants('images', response.data.shop.products.edges[i].node, productImages, promises);
+
+        // Fetch the rest of the variants for this product
+        this.fetchAllImagesOrVariants('variants', response.data.shop.products.edges[i].node, productVariants, promises);
+      }
+
+      return Promise.all(promises).then(() => {
+        return response.model.shop.products;
+      });
     });
   }
 
   fetchProduct(id, query = productQuery()) {
     return this.graphQLClient.send(query(this.graphQLClient, id)).then((response) => {
-      return response.model.node;
+      const promises = [];
+      const productImages = response.model.node.images;
+      const productVariants = response.model.node.variants;
+
+      // Fetch the rest of the images for this product
+      this.fetchAllImagesOrVariants('images', response.data.node, productImages, promises);
+
+      // Fetch the rest of the variants for this product
+      this.fetchAllImagesOrVariants('variants', response.data.node, productVariants, promises);
+
+      return Promise.all(promises).then(() => {
+        return response.model.node;
+      });
     });
   }
 
