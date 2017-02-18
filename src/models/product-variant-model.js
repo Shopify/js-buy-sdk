d6c791a88d56e928a376e0dfc5a7ccfcc766c2dd
Tessa Thornton <tessathornton@gmail.com>
2017-02-18 17:09:02 -0500

    fix variant checkoutUrl

--- src/models/product-variant-model.js
+++ src/models/product-variant-model.js
@@ -207,7 +207,7 @@ const ProductVariantModel = BaseModel.extend({
 
     const variantPath = `${this.id}:${parseInt(quantity, 10)}`;
 
-    const query = `api_key=${config.apiKey}`;
+    const query = `access_token=${config.apiKey}&_fd=0`;
 
     return `${baseUrl}/${variantPath}?${query}`;
   }
