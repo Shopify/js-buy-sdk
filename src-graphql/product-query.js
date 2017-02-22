e2a92d4b7f3f694f179cc23177b77578259c4175
Jessica Jiang <jessica.jiang@shopify.com>
2017-02-22 16:03:41 -0500

    Update schema and use descriptionHtml instead of bodyHtml

--- src-graphql/product-query.js
+++ src-graphql/product-query.js
@@ -4,7 +4,7 @@ import imageConnectionQuery from './image-connection-query';
 import variantConnectionQuery from './variant-connection-query';
 import addFields from './add-fields';
 
-const defaultFields = ['id', 'createdAt', 'updatedAt', 'bodyHtml', 'handle', 'productType', 'title', 'vendor', 'tags',
+const defaultFields = ['id', 'createdAt', 'updatedAt', 'descriptionHtml', 'descriptionPlainSummary', 'handle', 'productType', 'title', 'vendor', 'tags',
   'publishedAt', ['options', optionQuery()], ['images', imageConnectionQuery()], ['variants', variantConnectionQuery()]];
 
 export default function productQuery(fields = defaultFields) {
