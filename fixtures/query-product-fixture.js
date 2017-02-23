1a41c51e5f44b4a89e1eb3b94723d39c8d1e3d3c
Jessica Jiang <jessica.jiang@shopify.com>
2017-02-23 10:32:13 -0500

    Add query checking to tests

---
+++ fixtures/query-product-fixture.js
@@ -0,0 +1,24 @@
+export default {
+  "data": {
+    "shop": {
+      "products": {
+        "pageInfo": {
+          "hasNextPage": false,
+          "hasPreviousPage": false
+        },
+        "edges": [
+          {
+            "cursor": "eyJsYXN0X2lkIjo3ODU3OTg5Mzg0LCJsYXN0X3ZhbHVlIjoiNzg1Nzk4OTM4NCJ9",
+            "node": {
+              "id": "gid://shopify/Product/7857989384",
+              "updatedAt": "2017-02-17T19:56:02Z",
+              "createdAt": "2016-09-25T21:31:33Z",
+              "productType": "dog",
+              "title": "Cat"
+            }
+          }
+        ]
+      }
+    }
+  }
+};
