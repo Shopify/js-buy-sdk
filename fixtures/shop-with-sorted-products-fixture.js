a7388afb79608c9d6501ab11eba96a3108dc127f
Jessica Jiang <jessica.jiang@shopify.com>
2017-03-09 13:42:19 -0500

    Add sort option in query

---
+++ fixtures/shop-with-sorted-products-fixture.js
@@ -0,0 +1,35 @@
+export default {
+  "data": {
+    "shop": {
+      "products": {
+        "pageInfo": {
+          "hasNextPage": false,
+          "hasPreviousPage": false
+        },
+        "edges": [
+          {
+            "cursor": "eyJsYXN0X2lkIjo4NjMxNzQ5NTc2LCJsYXN0X3ZhbHVlIjoiMjAxNy0wMi0xNyAxOTo1ODowMCJ9",
+            "node": {
+              "id": "gid://shopify/Product/8631749576",
+              "updatedAt": "2017-02-17T19:58:00Z"
+            }
+          },
+          {
+            "cursor": "eyJsYXN0X2lkIjo4NTMwMDMzNTQ0LCJsYXN0X3ZhbHVlIjoiMjAxNy0wMi0xNyAxOTo1NjozMyJ9",
+            "node": {
+              "id": "gid://shopify/Product/8530033544",
+              "updatedAt": "2017-02-17T19:56:33Z"
+            }
+          },
+          {
+            "cursor": "eyJsYXN0X2lkIjo3ODU3OTg5Mzg0LCJsYXN0X3ZhbHVlIjoiMjAxNy0wMi0xNyAxOTo1NjowMiJ9",
+            "node": {
+              "id": "gid://shopify/Product/7857989384",
+              "updatedAt": "2017-02-17T19:56:02Z"
+            }
+          }
+        ]
+      }
+    }
+  }
+};
