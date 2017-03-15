4d74741adfccdf4a1a8c086d93992b12fe54b494
jzjiang <jessica.jiang@shopify.com>
2017-03-15 11:42:06 -0400

    Add test for nonexistent variants

--- src-graphql/product-helpers.js
+++ src-graphql/product-helpers.js
@@ -1,17 +1,9 @@
 export default class ProductHelpers {
   variantForOptions(product, options) {
-    const [selectedVariant] = product.variants.reduce((variantAcc, variant) => {
-      const match = variant.selectedOptions.every((selectedOption) => {
+    return product.variants.find((variant) => {
+      return variant.selectedOptions.every((selectedOption) => {
         return options[selectedOption.name] === selectedOption.value.valueOf();
       });
-
-      if (match) {
-        variantAcc.push(variant);
-      }
-
-      return variantAcc;
-    }, []);
-
-    return selectedVariant;
+    });
   }
 }
