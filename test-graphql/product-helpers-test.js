84c85645727afd0a19636c087ebe024035a4462b
jzjiang <jessica.jiang@shopify.com>
2017-03-15 11:42:06 -0400

    Add test for nonexistent variants

--- test-graphql/product-helpers-test.js
+++ test-graphql/product-helpers-test.js
@@ -7,21 +7,32 @@ import fetchMock from './isomorphic-fetch-mock'; // eslint-disable-line import/n
 
 suite('product-helpers-test', () => {
   const productHelpers = new ProductHelpers();
+  const config = new Config({
+    domain: 'single-product.myshopify.com',
+    storefrontAccessToken: 'abc123'
+  });
+
+  const client = new Client(config);
 
   test('it returns the variant based on options given', () => {
-    const config = new Config({
-      domain: 'single-product.myshopify.com',
-      storefrontAccessToken: 'abc123'
-    });
-
-    const client = new Client(config);
-
     fetchMock.postOnce('https://single-product.myshopify.com/api/graphql', singleProductFixture);
 
     return client.fetchProduct('7857989384').then((product) => {
       const variant = productHelpers.variantForOptions(product, {Fur: 'Fluffy', Size: 'Medium'});
 
       assert.equal(variant.id, 'gid://shopify/ProductVariant/25602235976');
+      assert.ok(fetchMock.done());
+    });
+  });
+
+  test('it returns undefined if the variant does not exist', () => {
+    fetchMock.postOnce('https://single-product.myshopify.com/api/graphql', singleProductFixture);
+
+    return client.fetchProduct('7857989384').then((product) => {
+      const variant = productHelpers.variantForOptions(product, {Fur: 'Fluffy', Size: 'Small'});
+
+      assert.equal(typeof variant, 'undefined');
+      assert.ok(fetchMock.done());
     });
   });
 });
