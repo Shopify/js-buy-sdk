e370790b44c9f6e38090e33534809da679da148b
jzjiang <jessica.jiang@shopify.com>
2017-03-15 14:20:55 -0400

    Move query root so collection and product connections can be nested

--- src-graphql/collection-connection-query.js
+++ src-graphql/collection-connection-query.js
@@ -4,13 +4,9 @@ import addFields from './add-fields';
 const defaultFields = ['id', 'handle', 'updatedAt', 'title', ['image', imageQuery()]];
 
 export default function collectionConnectionQuery(fields = defaultFields) {
-  return function(client) {
-    return client.query((root) => {
-      root.add('shop', (shop) => {
-        shop.addConnection('collections', {args: {first: 20}}, (collections) => {
-          addFields(collections, fields);
-        });
-      });
+  return function(parentSelection, fieldName) {
+    parentSelection.addConnection(fieldName, {args: {first: 20}}, (collections) => {
+      addFields(collections, fields);
     });
   };
 }
--- test-graphql/query-test.js
+++ test-graphql/query-test.js
@@ -7,6 +7,7 @@ import variantConnectionQuery from '../src-graphql/variant-connection-query';
 import optionQuery from '../src-graphql/option-query';
 import imageQuery from '../src-graphql/image-query';
 import imageConnectionQuery from '../src-graphql/image-connection-query';
+import collectionQuery from '../src-graphql/collection-query';
 
 suite('query-test', () => {
   const querySplitter = /[\s,]+/;
@@ -23,7 +24,13 @@ suite('query-test', () => {
   const client = new Client(config);
 
   test('it creates product queries with defaults', () => {
-    const query = productConnectionQuery();
+    const defaultQuery = productConnectionQuery();
+    const query = client.graphQLClient.query((root) => {
+      root.add('shop', (shop) => {
+        defaultQuery(shop, 'products');
+      });
+    });
+
     const queryString = `query {
       shop {
         products (first: 20) {
@@ -89,11 +96,17 @@ suite('query-test', () => {
       }
     }`;
 
-    assert.deepEqual(tokens(query(client.graphQLClient).toString()), tokens(queryString));
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
   });
 
   test('it creates product queries with specified fields', () => {
-    const query = productConnectionQuery(['id', 'tags', 'vendor', ['images', imageConnectionQuery(['src'])], ['options', optionQuery(['name'])], ['variants', variantConnectionQuery(['id', 'title'])]]);
+    const customQuery = productConnectionQuery(['id', 'tags', 'vendor', ['images', imageConnectionQuery(['src'])], ['options', optionQuery(['name'])], ['variants', variantConnectionQuery(['id', 'title'])]]);
+    const query = client.graphQLClient.query((root) => {
+      root.add('shop', (shop) => {
+        customQuery(shop, 'products');
+      });
+    });
+
     const queryString = `query {
       shop {
         products (first: 20) {
@@ -142,11 +155,17 @@ suite('query-test', () => {
       }
     }`;
 
-    assert.deepEqual(tokens(query(client.graphQLClient).toString()), tokens(queryString));
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
   });
 
   test('it creates collection queries with defaults', () => {
-    const query = collectionConnectionQuery();
+    const defaultQuery = collectionConnectionQuery();
+    const query = client.graphQLClient.query((root) => {
+      root.add('shop', (shop) => {
+        defaultQuery(shop, 'collections');
+      });
+    });
+
     const queryString = `query {
       shop {
         collections (first: 20) {
@@ -172,11 +191,18 @@ suite('query-test', () => {
       }
     }`;
 
-    assert.deepEqual(tokens(query(client.graphQLClient).toString()), tokens(queryString));
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
   });
 
   test('it creates collection queries with specified fields', () => {
-    const query = collectionConnectionQuery(['handle', 'updatedAt', 'title', ['image', imageQuery(['id'])]]);
+    const customQuery = collectionConnectionQuery(['handle', 'updatedAt', 'title', ['image', imageQuery(['id'])]]);
+    const query = client.graphQLClient.query((root) => {
+      root.add('shop', (shop) => {
+        customQuery(shop, 'collections');
+      });
+    });
+
+
     const queryString = `query {
       shop {
         collections (first: 20) {
@@ -200,6 +226,82 @@ suite('query-test', () => {
       }
     }`;
 
-    assert.deepEqual(tokens(query(client.graphQLClient).toString()), tokens(queryString));
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
+  });
+
+  test('it can create a nested product connection query', () => {
+    const customQuery = collectionQuery([['products', productConnectionQuery()]]);
+    const query = client.graphQLClient.query((root) => {
+      customQuery(root, 'node', '1');
+    });
+
+    const queryString = `query {
+      node (id: "gid://shopify/Collection/1") {
+        __typename,
+        ... on Collection {
+          id,products (first: 20) {
+            pageInfo {
+              hasNextPage,hasPreviousPage
+            },
+            edges {
+              cursor,
+              node {
+                id,
+                createdAt,
+                updatedAt,
+                descriptionHtml,
+                descriptionPlainSummary,
+                handle,
+                productType,
+                title,
+                vendor,
+                tags,
+                publishedAt,
+                options {
+                  id,
+                  name,
+                  values
+                },
+                images (first: 250) {
+                  pageInfo {
+                    hasNextPage,
+                    hasPreviousPage
+                  },
+                  edges {
+                    cursor,
+                    node {
+                      id,
+                      src,
+                      altText
+                    }
+                  }
+                },
+                variants (first: 250) {
+                  pageInfo {
+                    hasNextPage,
+                    hasPreviousPage
+                  },
+                  edges {
+                    cursor,
+                    node {
+                      id,
+                      title,
+                      price,
+                      weight,
+                      selectedOptions {
+                        name,
+                        value
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
   });
 });
