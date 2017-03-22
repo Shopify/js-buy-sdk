3ce5a0e1778b80124340f465485cf8aa476ed6ed
jzjiang <jessica.jiang@shopify.com>
2017-03-22 10:23:54 -0400

    Convert arrays to long form

--- src-graphql/checkout-query.js
+++ src-graphql/checkout-query.js
@@ -3,8 +3,17 @@ import shippingRateQuery from './shipping-rate-query';
 import mailingAddressQuery from './mailing-address-query';
 import baseQuery from './base-query';
 
-const defaultFields = ['id', 'ready', 'note', 'createdAt', 'updatedAt', 'requiresShipping', ['shippingLine', shippingRateQuery()],
-  ['shippingAddress', mailingAddressQuery()], ['lineItems', lineItemConnectionQuery()]];
+const defaultFields = [
+  'id',
+  'ready',
+  'note',
+  'createdAt',
+  'updatedAt',
+  'requiresShipping',
+  ['shippingLine', shippingRateQuery()],
+  ['shippingAddress', mailingAddressQuery()],
+  ['lineItems', lineItemConnectionQuery()]
+];
 
 export default function checkoutQuery(fields = defaultFields) {
   return baseQuery(fields);
--- src-graphql/mailing-address-query.js
+++ src-graphql/mailing-address-query.js
@@ -1,6 +1,24 @@
 import baseQuery from './base-query';
 
-const defaultFields = ['address1', 'address2', 'city', 'company', 'country', 'firstName', 'formatted', 'lastName', 'latitude', 'longitude', 'phone', 'province', 'zip', 'name', 'countryCode', 'provinceCode', 'id'];
+const defaultFields = [
+  'address1',
+  'address2',
+  'city',
+  'company',
+  'country',
+  'firstName',
+  'formatted',
+  'lastName',
+  'latitude',
+  'longitude',
+  'phone',
+  'province',
+  'zip',
+  'name',
+  'countryCode',
+  'provinceCode',
+  'id'
+];
 
 export default function mailingAddressQuery(fields = defaultFields) {
   return baseQuery(fields);
