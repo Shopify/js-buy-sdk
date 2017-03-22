948e3a46d1ce7deaae4bb162c56c7aa5198d1c65
jzjiang <jessica.jiang@shopify.com>
2017-03-22 10:23:54 -0400

    Convert arrays to long form

--- src-graphql/variant-query.js
+++ src-graphql/variant-query.js
@@ -2,7 +2,14 @@ import selectedOptionQuery from './selected-option-query';
 import imageQuery from './image-query';
 import baseQuery from './base-query';
 
-const defaultFields = ['id', 'title', 'price', 'weight', ['image', imageQuery()], ['selectedOptions', selectedOptionQuery()]];
+const defaultFields = [
+  'id',
+  'title',
+  'price',
+  'weight',
+  ['image', imageQuery()],
+  ['selectedOptions', selectedOptionQuery()]
+];
 
 export default function productQuery(fields = defaultFields) {
   return baseQuery(fields);
--- src/product-connection-query.js
+++ src/product-connection-query.js
@@ -3,8 +3,22 @@ import imageConnectionQuery from './image-connection-query';
 import variantConnectionQuery from './variant-connection-query';
 import addFields from './add-fields';
 
-const defaultFields = ['id', 'createdAt', 'updatedAt', 'descriptionHtml', 'descriptionPlainSummary', 'handle', 'productType', 'title', 'vendor', 'tags',
-  'publishedAt', ['options', optionQuery()], ['images', imageConnectionQuery()], ['variants', variantConnectionQuery()]];
+const defaultFields = [
+  'id',
+  'createdAt',
+  'updatedAt',
+  'descriptionHtml',
+  'descriptionPlainSummary',
+  'handle',
+  'productType',
+  'title',
+  'vendor',
+  'tags',
+  'publishedAt',
+  ['options', optionQuery()],
+  ['images', imageConnectionQuery()],
+  ['variants', variantConnectionQuery()]
+];
 
 export default function productConnectionQuery(fields = defaultFields) {
   return function(parentSelection, fieldName) {
