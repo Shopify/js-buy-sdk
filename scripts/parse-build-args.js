c1018e74c4aeef7edff2a8febbc303b49bbc00d2
Mina Smart <mina.smart@shopify.com>
2017-04-04 16:28:12 -0400

    Create a way to test with the optimized bundle

--- scripts/parse-build-args.js
+++ scripts/parse-build-args.js
@@ -3,14 +3,19 @@ const minimist = require('minimist');
 
 module.exports = function parseBuildArgs() {
   const args = minimist(process.argv.slice(2), {
-    boolean: 'with-dependency-tracking',
+    boolean: [
+      'with-dependency-tracking',
+      'with-optimized-types'
+    ],
     default: {
-      'with-dependency-tracking': false
+      'with-dependency-tracking': false,
+      'with-optimized-types': false
     }
   });
 
   const dest = args._[0];
   const withDependencyTracking = args['with-dependency-tracking'];
+  const withOptimizedTypes = args['with-optimized-types'];
 
-  return {dest, withDependencyTracking};
+  return {dest, withDependencyTracking, withOptimizedTypes};
 };
