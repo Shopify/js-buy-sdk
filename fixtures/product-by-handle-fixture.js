7f6a0a972ed37f354d78510b6f819c03c8b91bc2
Dennis Pacewicz <dennis.pacewicz@gmail.com>
2017-08-14 16:57:24 -0400

    fetch products + collections by handle (#399)
    * add function + query to fetch product by handle
    
    * add collection by handle
    
    * add tests for fetching product and collection by handle
    
    remove shop prefix from queries
    
    fix formatting + typo
    
    add docs to fetchCollectionByHandle

---
+++ fixtures/product-by-handle-fixture.js
@@ -0,0 +1,267 @@
+export default {
+  "data": {
+    "shop": {
+      "productByHandle": {
+        "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0Lzk1NTUyMjc0NjI=",
+        "createdAt": "2017-02-15T15:08:51Z",
+        "title": "Cape Dress",
+        "updatedAt": "2017-06-07T15:18:50Z",
+        "description": "This is a demonstration store. You can purchase products like this from Baby &amp; Company The Cape Dress from Co. captivates. The waistless silhouette offers a flattering look. Invisible zip closure at side. 82% Triacetate, 18% Polyester. Made in China. Ashley is wearing an X-Small.",
+        "descriptionHtml": "<meta charset=\"utf-8\">\n<p><em>This is a demonstration store. You can purchase products like this from <a href=\"http://babyandco.us/\" target=\"_blank\" class=\"text-link\" rel=\"noopener noreferrer\">Baby &amp; Company</a></em></p>\n<p>The Cape Dress from Co. captivates. The waistless silhouette offers a flattering look. Invisible zip closure at side. 82% Triacetate, 18% Polyester. Made in China. <em>Ashley is wearing an X-Small.</em></p>",
+        "handle": "cape-dress-1",
+        "productType": "women's dresses",
+        "vendor": "Co",
+        "tags": [
+          "arrivals",
+          "AW15",
+          "black",
+        ],
+        "publishedAt": "2017-02-15T15:09:45Z",
+        "options": [
+          {
+            "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0T3B0aW9uLzExNTI2MDQwNzc0",
+            "name": "Color",
+            "values": [
+              "Black",
+              "Dark Green"
+            ]
+          },
+          {
+            "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0T3B0aW9uLzExNTI2MDQwODM4",
+            "name": "Size",
+            "values": [
+              "X-Small",
+              "Small",
+              "Medium",
+              "Large"
+            ]
+          }
+        ],
+        "images": {
+          "pageInfo": { 
+            "hasNextPage": false, 
+            "hasPreviousPage": false 
+          },
+          "edges": [
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDgxNjY=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDgyMzA=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24008.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDgyOTQ=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24015.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDgzNTg=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24020.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg0MjI=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_34_51031_24037.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg0ODY=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24024.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg1NTA=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24034.jpg?v=1487171332",
+                "altText": null
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg2MTQ=",
+                "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_34_51031_24040.jpg?v=1487171332",
+                "altText": null
+              }
+            }
+          ]
+        },
+        "variants": {
+          "pageInfo": { 
+            "hasNextPage": false, 
+            "hasPreviousPage": false 
+          },
+          "edges": [
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NDIxNA==",
+                "available": true,
+                "weight": 0,
+                "price": "788.00",
+                "title": "Black / X-Small",
+                "image": null,
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Black"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "X-Small"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NDQwNg==",
+                "available": true,
+                "weight": 0,
+                "price": "788.00",
+                "title": "Black / Small",
+                "image": null,
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Black"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "Small"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NDUzNA==",
+                "available": false,
+                "weight": 0,
+                "price": "788.00",
+                "title": "Black / Medium",
+                "image": null,
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Black"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "Medium"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NDcyNg==",
+                "available": true,
+                "weight": 0,
+                "price": "750.00",
+                "title": "Dark Green / X-Small",
+                "image": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg0MjI=",
+                  "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_34_51031_24037.jpg?v=1487171332"
+                },
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Dark Green"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "X-Small"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NDkxOA==",
+                "available": false,
+                "weight": 0,
+                "price": "750.00",
+                "title": "Dark Green / Small",
+                "image": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg2MTQ=",
+                  "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_34_51031_24040.jpg?v=1487171332"
+                },
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Dark Green"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "Small"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zNTgzMTE3NTExMA==",
+                "available": true,
+                "weight": 0,
+                "price": "788.00",
+                "title": "Dark Green / Medium",
+                "image": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg1NTA=",
+                  "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24034.jpg?v=1487171332"
+                },
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Dark Green"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "Medium"
+                  }
+                ]
+              }
+            },
+            {
+              "node": {
+                "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MjgyNzA5NjcyNQ==",
+                "available": true,
+                "weight": 0,
+                "price": "788.00",
+                "title": "Dark Green / Large",
+                "image": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0SW1hZ2UvMjI5MzI5MDg0ODY=",
+                  "src": "https://cdn.shopify.com/s/files/1/1019/0495/products/2015-07-02_Ashley_33_51032_24024.jpg?v=1487171332"
+                },
+                "selectedOptions": [
+                  {
+                    "name": "Color",
+                    "value": "Dark Green"
+                  },
+                  {
+                    "name": "Size",
+                    "value": "Large"
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+};
