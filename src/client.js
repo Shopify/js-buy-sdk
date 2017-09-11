14871c521878a77676f024fee4374c2e659e08d1
Mina Smart <mina.smart@shopify.com>
2017-09-11 15:04:13 -0400

    resource-ify shop

--- src/client.js
+++ src/client.js
@@ -6,13 +6,12 @@ import imageHelpers from './image-helpers';
 import defaultResolver from './default-resolver';
 import ProductResource from './product-resource';
 import CollectionResource from './collection-resource';
+import ShopResource from './shop-resource';
 import {version} from '../package.json';
 
 // GraphQL
 import types from '../schema.json';
 import checkoutNodeQuery from './graphql/checkoutNodeQuery.graphql';
-import shopQuery from './graphql/shopQuery.graphql';
-import shopPolicyQuery from './graphql/shopPolicyQuery.graphql';
 import checkoutCreateMutation from './graphql/checkoutCreateMutation.graphql';
 import checkoutLineItemsAddMutation from './graphql/checkoutLineItemsAddMutation.graphql';
 import checkoutLineItemsRemoveMutation from './graphql/checkoutLineItemsRemoveMutation.graphql';
@@ -73,40 +72,7 @@ class Client {
 
     this.product = new ProductResource(this.graphQLClient, productHelpers);
     this.collection = new CollectionResource(this.graphQLClient);
-    console.log(this.collection);
-  }
-
-  /**
-   * Fetches shop information (`currencyCode`, `description`, `moneyFormat`, `name`, and `primaryDomain`).
-   * See the {@link https://help.shopify.com/api/storefront-api/reference/object/shop|Storefront API reference} for more information.
-   *
-   * @example
-   * client.fetchShopInfo().then((shop) => {
-   *   // Do something with the shop
-   * });
-   *
-   * @return {Promise|GraphModel} A promise resolving with a `GraphModel` of the shop.
-   */
-  fetchShopInfo() {
-    return this.graphQLClient
-      .send(shopQuery)
-      .then(defaultResolver('shop'));
-  }
-
-  /**
-   * Fetches shop policies (privacy policy, terms of service and refund policy).
-   *
-   * @example
-   * client.fetchShopPolicies().then((shop) => {
-   *   // Do something with the shop
-   * });
-   *
-   * @return {Promise|GraphModel} A promise resolving with a `GraphModel` of the shop.
-   */
-  fetchShopPolicies() {
-    return this.graphQLClient
-      .send(shopPolicyQuery)
-      .then(defaultResolver('shop'));
+    this.shop = new ShopResource(this.graphQLClient);
   }
 
   /**
