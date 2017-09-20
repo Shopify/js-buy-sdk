f2eb73923b7215dba7958ecc94d302f83f3cd7d1
Mina Smart <mina.smart@shopify.com>
2017-09-20 11:20:10 -0400

    extract common bits of test config

--- rollup-node-tests.config.js
+++ rollup-node-tests.config.js
@@ -1,67 +1,45 @@
-import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import multiEntry from 'rollup-plugin-multi-entry';
 import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
 import remap from 'rollup-plugin-remap';
-import graphqlCompiler from 'rollup-plugin-graphql-js-client-compiler';
-import eslintTestGenerator from './scripts/rollup-plugin-eslint-test-generator';
+import baseConfig from './rollup-tests-common.config';
 
-export default {
-  entry: ['test/setup.js', 'test/**/*-test.js'],
-  plugins: [
-    graphqlCompiler({
-      schema: './schema.json'
-    }),
-    multiEntry({
-      exports: false
-    }),
-    remap({
-      originalPath: './test/isomorphic-fetch-mock.js',
-      targetPath: './test/fetch-mock-node.js'
-    }),
-    json({
-      exclude: './schema.json'
-    }),
-    eslintTestGenerator({
-      paths: [
-        'src',
-        'test'
+baseConfig.plugins.push(
+  remap({
+    originalPath: './test/isomorphic-fetch-mock.js',
+    targetPath: './test/fetch-mock-node.js'
+  }),
+  nodeResolve({
+    jsnext: true,
+    main: true,
+    preferBuiltins: true
+  }),
+  babel({
+    babelrc: false,
+    presets: [
+      [
+        `${process.cwd()}/node_modules/babel-preset-shopify/node`, {
+          modules: false
+        }
       ]
-    }),
-    nodeResolve({
-      jsnext: true,
-      main: true,
-      preferBuiltins: true
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    }),
-    babel({
-      babelrc: false,
-      presets: [
-        [
-          `${process.cwd()}/node_modules/babel-preset-shopify/node`, {
-            modules: false
-          }
-        ]
-      ]
-    })
-  ],
-  targets: [{
-    format: 'cjs',
-    dest: '.tmp/test/node-tests.js'
-  }],
-  external: [
-    'assert',
-    'url',
-    'http',
-    'https',
-    'zlib',
-    'stream',
-    'buffer',
-    'util',
-    'string_decoder'
-  ],
-  sourceMap: true
-};
+    ]
+  })
+);
+
+baseConfig.targets = [{
+  format: 'cjs',
+  dest: '.tmp/test/node-tests.js'
+}];
+
+baseConfig.external = [
+  'assert',
+  'url',
+  'http',
+  'https',
+  'zlib',
+  'stream',
+  'buffer',
+  'util',
+  'string_decoder'
+];
+
+export default baseConfig;
