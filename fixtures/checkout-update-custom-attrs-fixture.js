ed73448c753ffd20eb283d4c7ee8576466fa8d7a
Stefan Slehta <stefan.slehta@shopify.com>
2018-06-18 11:51:48 -0400

    Add support for checkout update attributes

---
+++ fixtures/checkout-update-custom-attrs-fixture.js
@@ -0,0 +1,80 @@
+export default {
+  "data": {
+    "checkoutAttributesUpdate": {
+      "userErrors": [],
+      "checkout": {
+        "id": "Z2lkOi8vU2hvcGlmeS9FeGFtcGxlLzE=",
+        "ready": true,
+        "lineItems": {
+          "pageInfo": {
+            "hasNextPage": false,
+            "hasPreviousPage": false
+          },
+          "edges": [
+            {
+              "cursor": "eyJsYXN0X2lkIjoiZDUyZWU5ZTEwYmQxMWE0NDlkNmQzMWNkMzBhMGFjNzMifQ==",
+              "node": {
+                "title": "Intelligent Granite Table",
+                "variant": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yOTEwNjA2NDU4NA==",
+                  "title": "Awesome Copper Bench",
+                  "price": "64.99",
+                  "weight": 4.5,
+                  "image": null,
+                  "selectedOptions": [
+                    {
+                      "name": "Color or something",
+                      "value": "Awesome Copper Bench"
+                    }
+                  ]
+                },
+                "quantity": 5,
+                "customAttributes": []
+              }
+            }
+          ]
+        },
+        "shippingAddress": {
+          "address1": "123 Cat Road",
+          "address2": null,
+          "city": "Cat Land",
+          "company": "Catmart",
+          "country": "Canada",
+          "firstName": "Meow",
+          "formatted": [
+            "Catmart",
+            "123 Cat Road",
+            "Cat Land ON M3O 0W1",
+            "Canada"
+          ],
+          "lastName": "Meowington",
+          "latitude": null,
+          "longitude": null,
+          "phone": "4161234566",
+          "province": "Ontario",
+          "zip": "M3O 0W1",
+          "name": "Meow Meowington",
+          "countryCode": "CA",
+          "provinceCode": "ON",
+          "id": "Z2lkOi8vc2hvcGlmeS9QcmdfnAU8nakdWMnAbh890hyOTEwNjA2NDU4NA=="
+        },
+        "shippingLine": null,
+        "requiresShipping": true,
+        "customAttributes": [{key: "MyKey", value: "MyValue"}],
+        "note": null,
+        "paymentDue": "367.19",
+        "webUrl": "https://checkout.myshopify.io/1/checkouts/c4abf4bf036239ab5e3d0bf93c642c96",
+        "orderStatusUrl": null,
+        "taxExempt": false,
+        "taxesIncluded": false,
+        "currencyCode": "CAD",
+        "totalTax": "42.24",
+        "subtotalPrice": "324.95",
+        "totalPrice": "367.19",
+        "completedAt": null,
+        "createdAt": "2017-03-28T16:58:31Z",
+        "updatedAt": "2017-03-28T16:58:31Z"
+      }
+    }
+  }
+};
