9bd4908374f699f866a11bbb95693868ad787834
Darryl Norris <admin@darrylnorris.com>
2018-06-27 12:05:53 -0700

    Add support for Client.checkout#updateShippingAddress

---
+++ fixtures/checkout-shipping-address-update-v2-fixture.js
@@ -0,0 +1,71 @@
+export default {
+  "data": {
+    "checkoutShippingAddressUpdateV2": {
+      "userErrors": [],
+      "checkout": {
+        "id": "Z2lkOi8vc2hvcGlmeS9DaGVja291dC9lM2JkNzFmNzI0OGM4MDZmMzM3MjVhNTNlMzM5MzFlZj9rZXk9NDcwOTJlNDQ4NTI5MDY4ZDFiZTUyZTUwNTE2MDNhZjg=",
+        "createdAt": "2017-03-17T16:00:40Z",
+        "updatedAt": "2017-03-17T16:00:40Z",
+        "requiresShipping": true,
+        "shippingLine": null,
+        "order": null,
+        "orderStatusUrl": null,
+        "taxExempt": false,
+        "taxesIncluded": false,
+        "currencyCode": "CAD",
+        "totalPrice": "80.28",
+        "subtotalPrice": "67.50",
+        "totalTax": "8.78",
+        "paymentDue": "80.28",
+        "taxExempt": false,
+        "taxesIncluded": false,
+        "completedAt": null,
+        "shippingAddress": {
+          "id": "Z2lkOi8vc2hvcGlmeS9NYWlsaW5nQWRkcmVzcy8xMTAyNDgxNzE5MzE4P21vZGVsX25hbWU9QWRkcmVzcw==",
+          "address1": "Chestnut Street 92",
+          "address2": "Apartment 2",
+          "city": "Louisville",
+          "company": null,
+          "country": "United States",
+          "firstName": "Bob",
+          "formatted": [
+            "Chestnut Street 92",
+            "Apartment 2",
+            "Louisville KY 40202",
+            "United States"
+          ],
+          "lastName": "Norman",
+          "latitude": null,
+          "longitude": null,
+          "phone": "555-625-1199",
+          "province": "Kentucky",
+          "zip": "40202",
+          "name": "Bob Norman",
+          "countryCode": "US",
+          "provinceCode": "KY"
+        },
+        "lineItems": {
+          "pageInfo": {
+            "hasNextPage": false,
+            "hasPreviousPage": false
+          },
+          "edges": [
+            {
+              "cursor": "eyJsYXN0X2lkIjoiZDUyZWU5ZTEwYmQxMWE0NDlkNmQzMWNkMzBhMGFjNzMifQ==",
+              "node": {
+                "title": "Intelligent Granite Table",
+                "variant": {
+                  "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yOTEwNjA2NDU4NA==",
+                  "price": "74.99"
+                },
+                "quantity": 5,
+                "customAttributes": [],
+                "discountAllocations": []
+              }
+            }
+          ]
+        }
+      }
+    }
+  }
+};
