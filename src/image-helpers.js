78cbf8e4bf5dffef8d632ce713cbdd115a15de67
Jim Tattersall <jim@studiorotate.com>
2018-07-01 00:16:48 +0100

    Update Image Helper to support crop, scale and format options.

--- src/image-helpers.js
+++ src/image-helpers.js
@@ -4,32 +4,62 @@
 export default {
 
   /**
-   * Generates the image src for a resized image with maximum dimensions `maxWidth` and `maxHeight`.
+   * Generates the image src for a resized image with  `maxWidth`, `maxHeight`, `crop`, `scale` and `format` options.
    * Images do not scale up.
    *
    * @example
-   * const url = client.image.helpers.imageForSize(product.variants[0].image, {maxWidth: 50, maxHeight: 50});
+   * const url = client.image.helpers.imageForSize(product.variants[0].image, {
+   *  maxWidth: 50,
+   *  maxHeight: 50,
+   *  crop: 'center',
+   *  scale: 2,
+   *  format: 'pjpg'
+   * });
    *
    * @memberof ImageHelpers
    * @method imageForSize
    * @param {Object} image The original image model to generate the image src for.
    * @param {Object} options An options object containing:
-   *  @param {Integer} options.maxWidth The maximum width for the image.
-   *  @param {Integer} options.maxHeight The maximum height for the image.
+   * @param {Integer} options.maxWidth The maximum width for the image.
+   * @param {Integer} options.maxHeight The maximum height for the image.
+   * @param {String} options.crop The crop to determine the returned part of the image. Available values are `top`, `center`, `bottom`, `left`, `right`.
+   * @param {Integer} options.scale The pixel density for the image. Available values are 2 and 3.
+   * @param {String} options.format The format for the image. Available values are `jpg` and `pjpg`.
+
+
    * @return {String} The image src for the resized image.
    */
-  imageForSize(image, {maxWidth, maxHeight}) {
+  imageForSize(image, {maxWidth, maxHeight, crop, scale, format}) {
     const splitUrl = image.src.split('?');
-    const notQuery = splitUrl[0];
+    const withoutQuery = splitUrl[0];
     const query = splitUrl[1] ? `?${splitUrl[1]}` : '';
 
     // Use the section before the query
-    const imageTokens = notQuery.split('.');
+    const imageTokens = withoutQuery.split('.');
+
+    const queryBuilder = [];
+
+    // Adds the dimensions query if either maxWidth or maxHeight exists.
+    queryBuilder.push((maxWidth || maxHeight) ? `_${maxWidth}x${maxHeight}` : '');
+
+    // Adds the crop query if it exists.
+    queryBuilder.push(crop ? `_crop_${crop}` : '');
+
+    // Adds the scale query if it exists.
+    queryBuilder.push(scale ? `@${scale}x` : '');
+
+    // Makes the image progressive if passed as an option.
+    queryBuilder.push(format === 'progressive' ? '.progressive' : '');
+
+    // Adjusts the image format to jpg if the option is passed.
+    const lastImageToken = imageTokens.length - 1;
+
+    imageTokens[lastImageToken] = (format && imageTokens[lastImageToken] !== 'jpg') ? `${imageTokens[lastImageToken]}.jpg` : imageTokens[lastImageToken];
 
     // Take the token before the file extension and append the dimensions
     const imagePathIndex = imageTokens.length - 2;
 
-    imageTokens[imagePathIndex] = `${imageTokens[imagePathIndex]}_${maxWidth}x${maxHeight}`;
+    imageTokens[imagePathIndex] = `${imageTokens[imagePathIndex]}${queryBuilder.join('')}`;
 
     return `${imageTokens.join('.')}${query}`;
   }
--- test/image-helpers-test.js
+++ test/image-helpers-test.js
@@ -20,6 +20,36 @@ suite('image-helpers-test', () => {
     assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/1019/0495/products/Ashley.34.51031.test_280x560.jpg?v=1505141764');
   });
 
+  test('it returns the image src with the specified width, height and crop', () => {
+    const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5.jpg?v=1487171332'}, {maxWidth: 280, maxHeight: 560, crop: 'center'});
+
+    assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5_280x560_crop_center.jpg?v=1487171332');
+  });
+
+  test('it returns the image src with the specified width, height, crop and scale', () => {
+    const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5.jpg?v=1487171332'}, {maxWidth: 280, maxHeight: 560, crop: 'center', scale: 2});
+
+    assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5_280x560_crop_center@2x.jpg?v=1487171332');
+  });
+
+  test('it returns the image src with the specified width, height, crop, scale and format', () => {
+    const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5.jpg?v=1487171332'}, {maxWidth: 280, maxHeight: 560, crop: 'center', scale: 2, format: 'progressive'});
+
+    assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5_280x560_crop_center@2x.progressive.jpg?v=1487171332');
+  });
+
+  test('it returns the image src with jpg format', () => {
+    const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/2446/9977/files/tracksmith-logo.png?11120365955370323516'}, {format: 'jpg'});
+
+    assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/2446/9977/files/tracksmith-logo.png.jpg?11120365955370323516');
+  });
+
+  test('it returns the image src with jpg format', () => {
+    const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/2446/9977/files/tracksmith-logo.png?11120365955370323516'}, {format: 'progressive'});
+
+    assert.equal(resizedImageSrc, 'https://cdn.shopify.com/s/files/1/2446/9977/files/tracksmith-logo.progressive.png.jpg?11120365955370323516');
+  });
+
   test('it returns the correct url for an image src with . in the query param', () => {
     const resizedImageSrc = imageHelpers.imageForSize({src: 'https://cdn.shopify.com/s/files/1/1019/0495/products/babyandco5.jpg?v=148.71713.32'}, {maxWidth: 280, maxHeight: 560});
 
