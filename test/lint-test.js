44e38454ad1412a798f1e25a56155458f788c953
Rebecca Friedman <rebecca.friedman@shopify.com>
2018-10-24 10:07:43 -0400

    Validate that all Connections include PageInfo

--- test/lint-test.js
+++ test/lint-test.js
@@ -1,1 +1,22 @@
 import 'lint-tests'; // eslint-disable-line
+
+import assert from 'assert';
+import Client from '../src/client';
+
+suite('manual-lint-test', () => {
+  const config = {
+    domain: 'sendmecats.myshopify.com',
+    storefrontAccessToken: 'abc123'
+  };
+
+  test('it ensures that all Connections include pageInfo', () => {
+    const client = Client.buildClient(config);
+    const objectTypes = client.graphQLClient.typeBundle.types;
+
+    for (const key in objectTypes) {
+      if (objectTypes.hasOwnProperty(key) && key.includes('Connection')) {
+        assert.equal(objectTypes[key].fieldBaseTypes.pageInfo, 'PageInfo');
+      }
+    }
+  });
+});
