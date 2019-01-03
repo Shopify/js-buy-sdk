4b72a9169687462aa9e3fe4a982027c1c0661f14
Rebecca Friedman <rebecca.friedman@shopify.com>
2019-01-03 15:54:01 -0500

    Remove deprecated currencyCode field

--- src/shop-resource.js
+++ src/shop-resource.js
@@ -12,7 +12,7 @@ import shopPolicyQuery from './graphql/shopPolicyQuery.graphql';
 class ShopResource extends Resource {
 
   /**
-   * Fetches shop information (`currencyCode`, `description`, `moneyFormat`, `name`, and `primaryDomain`).
+   * Fetches shop information (`paymentSettings` (`currencyCode`), `description`, `moneyFormat`, `name`, and `primaryDomain`).
    * See the {@link https://help.shopify.com/api/storefront-api/reference/object/shop|Storefront API reference} for more information.
    *
    * @example
