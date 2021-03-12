839d24c8e8f4a6f7bfff61707cd03b2dc22f98c5
Isamu Arimoto <isamu@to-kyo.to>
2021-03-12 10:09:06 +0900

    add customerResource

---
+++ src/customer-resource.js
@@ -0,0 +1,39 @@
+import Resource from './resource';
+import defaultResolver from './default-resolver';
+
+// GraphQL
+import customerCreateMutation from './graphql/customerCreateMutation.graphql';
+
+/**
+ * The JS Buy SDK customer resource
+ * @class
+ */
+class CustomerResource extends Resource {
+
+  /**
+   * Creates a customer.
+   *
+   * @example
+   * const input = {
+   *   email: "isamu@to-kyo.to",
+   *   password: "HiZqFuDvDdQ7"
+   * };
+   *
+   * client.customer.create(input).then((customer) => {
+   *   // Do something with the newly created customer
+   * });
+   *
+   * @param {Object} [input] An input object containing of:
+   *   @param {String} [input.email] The customer’s email
+   *   @param {String} [input.password] The login password used by the customer.
+   * @return {Promise|GraphModel} A promise resolving with the created customer.
+   */
+  create(input) {
+    return this.graphQLClient
+      .send(customerCreateMutation, {input})
+      .then(defaultResolver('customerCreate.customer'));
+  }
+
+}
+
+export default CustomerResource;
